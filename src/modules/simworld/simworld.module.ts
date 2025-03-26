77cf768320c48b687ef4ed790634d4e5541a9cf3
s2s2hyun <s2s2hyun0601@gmail.com>
2025-03-26 13:31:00 +0900

    [Fix] cc

--- src/modules/simworld/simworld.module.ts
+++ src/modules/simworld/simworld.module.ts
@@ -1,10 +1,8 @@
-import { TcpModule } from '@modules/tcp/tcp.module';
 import { Module } from '@nestjs/common';
 import { SimworldController } from './simworld.controller';
 import { SimworldService } from './simworld.service';
 
 @Module({
-  imports: [TcpModule],
   controllers: [SimworldController],
   providers: [SimworldService],
 })
