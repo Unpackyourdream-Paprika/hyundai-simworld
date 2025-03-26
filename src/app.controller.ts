ff48ff25bf31ccc38c84a28c424102c2455663e0
juzdalua <bearwcw@naver.com>
2025-03-26 14:30:33 +0900

    Feat: Remove root controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,10 +4,4 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    throw new HttpException("error", HttpStatus.INTERNAL_SERVER_ERROR);
-    return this.appService.getHello();
-  }
 }
